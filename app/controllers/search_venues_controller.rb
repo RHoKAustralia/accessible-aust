e11f6750b298d9abfb36bf745158966868e2dd12
João Medrado <jcarloz@gmail.com>
2016-06-04 16:01:44 +1000

    basic API search for disability type and location

---
+++ app/controllers/search_venues_controller.rb
@@ -0,0 +1,16 @@
+class SearchVenuesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:create]
+
+  def create
+    disability_condition = disability_search(params['disability_type'])
+    render json: Venue.where(disability_condition).where('location LIKE :location', location: "%#{params['location']}%")
+  end
+
+  def disability_search(disability_type)
+    disability_types = ['m1', 'm2', 'hs', 'h1', 'v1']
+    unless disability_types.include?(disability_type)
+      fail ArgumentError, "Invalid disability_type #{disability_type}"
+    end
+    {disability_type.to_sym => 'YES'}
+  end
+end
---
+++ test/controllers/search_venues_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SearchVenuesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
